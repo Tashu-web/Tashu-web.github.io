307c067b7e3f2a60341d3e07ac822e3b2228d8cd
FlameGame-maker <madest171@gmail.com>
2025-11-14 19:39:37 +0300

    New commit

---
+++ src/5/script.js
@@ -0,0 +1,15 @@
+﻿// Инициализация после загрузки DOM
+document.addEventListener('DOMContentLoaded', function () {
+    // Получение элементов DOM
+    const productSelect = document.getElementById('product');
+    const quantityInput = document.getElementById('quantity');
+    const calculateBtn = document.getElementById('calculate-btn');
+    const resultDiv = document.getElementById('result');
+
+    calculateBtn.addEventListener('click', function () {
+        const productPrice = parseInt(productSelect.value, 10);
+        const quantity = parseInt(quantityInput.value.trim(), 10);
+        const totalCost = productPrice * quantity;
+        resultDiv.textContent = `${totalCost} ₽`;
+    });
+});
