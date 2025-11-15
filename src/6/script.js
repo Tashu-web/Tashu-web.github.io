6af4860d84004ace421fcff62b673fc63f99176e
Ташу Мадин Юрьевич <s0195860@fpm.edu>
2025-11-15 10:46:09 +0300

    New commit

---
+++ src/6/script.js
@@ -0,0 +1,18 @@
+﻿// Инициализация после загрузки DOM
+document.addEventListener('DOMContentLoaded', function () {
+    // Получение элементов DOM
+    const productSelect = document.getElementById('product');
+    const quantityInput = document.getElementById('quantity');
+    const calculateBtn = document.getElementById('calculate-btn');
+    const resultDiv = document.getElementById('result');
+
+    calculateBtn.addEventListener('click', function () {
+        const quantity = parseInt(quantityInput.value.trim(), 10);
+
+        if (Number.isInteger(quantity) && quantity >= 0) {
+            const productPrice = parseInt(productSelect.value, 10);
+            const totalCost = productPrice * quantity;
+            resultDiv.textContent = `${totalCost} ₽`;
+        } else resultDiv.textContent = `Недействительное значение количества`;       
+    });
+});
